81d038cd320ebc8fe17206d4e05d226f835d32d1
s17245 <s17245@pjwstk.edu.pl>
2020-03-01 18:04:52 +0100

    @ graber

--- Ćwiczenie_1/Ćwiczenie_1/Program.cs
+++ Ćwiczenie_1/Ćwiczenie_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Ćwiczenie_1
@@ -13,7 +14,14 @@ namespace Ćwiczenie_1
             var result = await client.GetAsync("https://www.pja.edu.pl");
 
             if (result.IsSuccessStatusCode) {
+
                 string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z0-9]+@[a-z0-9]+\\.[\\.a-z]+", RegexOptions.IgnoreCase);
+
+                var matches = regex.Matches(html);
+                foreach (var m in matches) {
+                    Console.WriteLine(m);
+                }
             }
 
 
