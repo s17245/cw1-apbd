ff86da0dc93e34a13c2fbb1915e194cd6abbf9e8
s17245 <s17245@pjwstk.edu.pl>
2020-03-01 17:56:15 +0100

    web grabber @

--- Ćwiczenie_1/Ćwiczenie_1/Program.cs
+++ Ćwiczenie_1/Ćwiczenie_1/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Ćwiczenie_1
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("start");
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode) {
+                string html = await result.Content.ReadAsStringAsync();
+            }
+
+
+            Console.WriteLine("End!");
         }
     }
 }
