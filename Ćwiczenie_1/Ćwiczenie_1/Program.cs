a480f836bf99b961c05ede2c1a173ed866e7e781
s17245 <s17245@pjwstk.edu.pl>
2020-03-01 17:39:25 +0100

    dodałem hello world

---
+++ Ćwiczenie_1/Ćwiczenie_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ćwiczenie_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
